e41197cb95aef3a97ae07d85b07dacf63a6b0f1a
Victor <victorbourgin@hotmail.com>
2020-02-09 19:03:48 +0000

    add latent prior network

--- dataset_faces.py
+++ dataset_faces.py
@@ -1,16 +1,30 @@
-import numpy as np
 import os
 import cv2
+import torch
+import numpy as np
 from torch.utils.data import Dataset
 
+##
+#train_num_persons = 1595-200
+#test_num_persons = 100
+#num_frames_per_dataset=5
+#data_dir = "C:/Users/Victor/Documents/Cambridge/Lent/MLMI4/Project/YouTubeFaces/resized_images_DB/"
+##
 
+#def get_dataset(split='train'):
 def get_dataset(opts, split='train'):
     """Function to get instance of FacesDataset given training options."""
+    #splits = {'train': slice(0, train_num_persons),
+    #          'test': slice(train_num_persons, train_num_persons + test_num_persons),
+    #          'valid': slice(train_num_persons + test_num_persons, 1595)
+    #         }
+
     splits = {'train': slice(0, opts.train_num_persons),
               'test': slice(opts.train_num_persons, opts.train_num_persons + opts.test_num_persons),
               'valid': slice(opts.train_num_persons + opts.test_num_persons, 1595)
              }
 
+#    return FacesDataset(data_dir, slice(0, 30), num_frames_per_dataset)
     return FacesDataset(opts.data_dir, splits[split], opts.num_frames_per_dataset)
 
 
@@ -28,26 +42,33 @@ class FacesDataset(Dataset):
         self.num_frames_per_dataset = num_frames_per_dataset
 
         self.videos = []  # name for each video
-        self.video_paths = []  # path for each video
+        datasets = []
         # Take each video for each person in dataset, and save both unique video name and path
+
+        show_im = True
         for person in self.persons:
             for video in os.listdir(os.path.join(self.data_dir, person)):
                 self.videos.append(person + "_" + video)
-                self.video_paths.append(os.path.join(self.data_dir, person, video))
+                video_path = os.path.join(self.data_dir, person, video)
+
+                # Sample num_frames_per_dataset frames from the given video, without replacement.
+                dataset_frames = np.random.choice(os.listdir(os.path.join(self.data_dir, person, video)),
+                                                  size=self.num_frames_per_dataset, replace=False)
+                dataset = []
+                for frame in dataset_frames:
+                    frame_path = os.path.join(video_path, frame)
+
+                    img_scaled = cv2.imread(frame_path, cv2.IMREAD_UNCHANGED).astype(np.float64)/255
+                    # Image must be size (num_channels, w, h) --> transpose, and append to list of images
+                    dataset.append(img_scaled.transpose(2, 0, 1))
+
+                dataset = np.array(dataset)  # Shape is (num_frames_per_dataset, 3, 64, 64)
+                datasets.append(dataset)  # Shape is (num_datasets, num_frames_per_dataset, 3, 64, 64)
+
+        self.dataset = np.array(datasets)
 
     def __len__(self):
         return len(self.videos)
 
     def __getitem__(self, idx):
-        video_path = self.video_paths[idx]
-
-        # Select num_frames_per_dataset from the given video, without replacement.
-        dataset_frames = np.random.choice(os.listdir(video_path), size=self.num_frames_per_dataset, replace=False)
-        dataset = []
-        for frame in dataset_frames:
-            img_scaled = cv2.imread(os.path.join(video_path, frame), cv2.IMREAD_UNCHANGED).astype(np.float64)/255
-            # Image must be size (num_channels, w, h) --> transpose, and append to list of images
-            dataset.append(img_scaled.transpose(2, 0, 1))
-
-        # Return dataset as array of size (n_frames, num_channels, 64, 64)
-        return np.array(dataset)
+        return {'datasets': torch.FloatTensor(self.dataset[idx])}
