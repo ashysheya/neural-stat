25146c2de725bdf47e9a53e1a167cca0ac82c11a
A. Shysheya <as2975@mlsalt-cpu2.eng.cam.ac.uk>
2020-02-18 14:23:04 +0000

    omniglot final version

---
+++ train_omniglot.sh
@@ -0,0 +1,2 @@
+python train.py --experiment omniglot --nll bernoulli --num_data_per_dataset 5 --num_epochs 400 \
+--context_dim 512 --masked --z_dim 16 --h_dim 4096 --batch_size 32 --lr 0.0001 --tensorboard
